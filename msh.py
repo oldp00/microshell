a1683a9e4694aa6ac4f6a106333a95108a59339e
danidoco <oldp79@gmail.com>
2019-08-16 16:50:47 +0900

    Add : command parsing code

--- msh.py
+++ msh.py
@@ -6,4 +6,7 @@ print("Windows MicroShell v0.0.1 alpha")
 while True:
     cmd = input("microshell [" + username + "] ~ ")
 
+    cmdBody = cmd.split(" ")[2]
+    cmdAttr = cmd.split(" ")[1]
+    
     # TODO: Add command execute engine
