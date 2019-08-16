2c0b3a7173a8de9433c53d771a5f8e03d4e074dd
danidoco <oldp79@gmail.com>
2019-08-16 16:40:36 +0900

    Comment : TODO

--- msh.py
+++ msh.py
@@ -1,7 +1,9 @@
 import getpass
 username = getpass.getuser()
 
-print("Windows MicroShell v0.0.1 alpha\n")
+print("Windows MicroShell v0.0.1 alpha")
 
 while True:
     cmd = input("microshell [" + username + "] ~ ")
+
+    # TODO: Add command execute engine
