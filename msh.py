ed0c70d71c2c9b4d9c1e862d7185a19674c3dd5b
danidoco <oldp79@gmail.com>
2019-08-23 22:28:48 +0900

    Feature: command "poweroff"

--- msh.py
+++ msh.py
@@ -34,6 +34,10 @@ while True:
         elif cmdBody == "getcd":
             os.system("echo %cd%")
 
+        elif cmdBody == "poweroff":
+            user32 = ctypes.WinDLL('user32')
+            user32.ExitWindowsEx(0x00000008, 0x00000000)
+            
         elif cmd.strip() == "":
             pass
 
