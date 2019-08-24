691156a95da649b9d2ec92f65d955573d4c12c8b
danidoco <oldp79@gmail.com>
2019-08-25 01:04:11 +0900

    Update: list index in beep command

--- msh.py
+++ msh.py
@@ -48,7 +48,7 @@ while True:
             os.system("powershell")
 
         elif cmdBody == "beep":
-            winsound.Beep(int(cmdAttrs[1]), int(cmdAttrs[2]))
+            winsound.Beep(int(cmdAttrs[0]), int(cmdAttrs[1]))
 
         elif cmdBody == "help":
             print("\necho  echoconf  clear  getcd  poweroff\nsetcolor  cmd  psh\n")
